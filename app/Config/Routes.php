48deb7bee954dd40fd79ec2aceb0e6bc105a9eb1
Nimeshika2025 <jayaniwickramasingha27@gmail.com>
2026-03-11 08:54:12 +0530

    Updated login form

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,3 +14,6 @@ $routes->get('foods', 'Menu::foods');
 $routes->get('foods/delete/(:num)', 'Menu::delete/$1');
 $routes->get('foods/edit/(:num)', 'Menu::edit/$1');
 $routes->post('foods/update/(:num)', 'Menu::update/$1');
+$routes->get('/login', 'Login::login');
+$routes->post('/login', 'Login::checkLogin');
+$routes->get('/logout', 'Login::logout');
